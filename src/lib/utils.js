14683f5ec20c0efc50230892caa27519b06565b9
homeshghorse2005 <homeshghorse@gmail.com>
2025-12-07 21:11:24 +0530

    fix issue

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,15 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
+  // Set cookie for browser
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    httpOnly: true, // cannot access in JS
+    secure: process.env.NODE_ENV === "production", // HTTPS only
+    sameSite: "none", // cross-site cookie
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
   return token;
