f07ecf44b9936427349726e39a24f0ac6dc9c905
homeshghorse2005 <homeshghorse@gmail.com>
2025-12-10 18:57:55 +0530

    Fix issue

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,13 +1,11 @@
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
-
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true,       // required on HTTPS
-    sameSite: "none",   // cross-site
+    secure: true,      // must be true on Vercel HTTPS
+    sameSite: "none",  // cross-domain cookie
     path: "/",
     maxAge: 7*24*60*60*1000,
   });
-
   return token;
 };
